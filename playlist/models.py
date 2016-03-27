329f8f418af1aea2cc3164dfca07ff51b684d181
ochuykin <lastvar@gmail.com>
2016-03-27 23:49:26 +0300

    add sort, filter, create audiotrack 2

--- playlist/models.py
+++ playlist/models.py
@@ -1,12 +1,25 @@
+# coding: utf-8
+
 from __future__ import unicode_literals
 from django.db import models
 
 
 class Playlist(models.Model):
 
-    name = models.CharField(max_length=255)
+    name = models.CharField(verbose_name=u'Название плейлиста', max_length=255)
 
-    audiotracks = models.ManyToManyField('audiotrack.Audiotrack', related_name="playlists")
+    audiotracks = models.ManyToManyField('audiotrack.Audiotrack', verbose_name=u'Список треков', related_name="playlists")
+
+    created_at = models.DateTimeField(verbose_name=u'Дата создания', auto_now_add=True)
+    changed_at = models.DateTimeField(verbose_name=u'Дата последнего изменения', auto_now_add=True)
+
+    is_private = models.BooleanField(default=True, verbose_name=u'Закрытый плейлист')
 
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name = u'Плейлист'
+        verbose_name_plural = u'Плейлисты'
+        ordering = ('-name', )
+
--- playlist/views.py
+++ playlist/views.py
@@ -16,3 +16,11 @@ class PlaylistList(ListView):
 
     model = Playlist
     template_name = 'playlist/playlists.html'
+
+
+    # def dispatch(self, request, *args, **kwargs):
+    #     self.sort_field = request.GET.get('sort_field')
+    #     return super(PlaylistList, self).dispatch(request, *args, **kwargs)
+    #
+    # def get_queryset(self):
+    #     return Playlist.objects.all().order_by(self.sort_field)[:10]
