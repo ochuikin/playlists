c07bb926850df5d637ede8e26962dfef3577d0d2
ochuykin <aq.prots@gmail.com>
2016-04-05 21:59:24 +0300

    working player

--- core/static/core/js/jaudio.js
+++ core/static/core/js/jaudio.js
@@ -416,31 +416,3 @@
   }
 
 })(jQuery)
-
-var t = {
-  playlist: [
-    {
-      file: "resources/tracks/01.mp3",
-      thumb: "resources/thumbs/01.jpg",
-      trackName: "Dusk",
-      trackArtist: "Tobu & Syndec",
-      trackAlbum: "Single",
-    },
-    {
-      file: "resources/tracks/02.mp3",
-      thumb: "resources/thumbs/02.jpg",
-      trackName: "Blank",
-      trackArtist: "Disfigure",
-      trackAlbum: "Single",
-    },
-    {
-      file: "resources/tracks/03.mp3",
-      thumb: "resources/thumbs/03.jpg",
-      trackName: "Fade",
-      trackArtist: "Alan Walker",
-      trackAlbum: "Single",
-    }
-  ]
-}
-
-$(".jAudio--player").jAudio(t);
